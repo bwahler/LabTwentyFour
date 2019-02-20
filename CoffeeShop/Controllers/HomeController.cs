c724d6d8dc216f1e1e3c6e3ae8394d83e57c74e0
bwahler <bmwahler@gmail.com>
2019-02-20 15:41:34 -0500

    Initial Commit

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using CoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "We make coffee we can be proud of!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "We would be happy to answer any questions or concerns you might have!";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Welcome(User u)
+        {
+            if (Session["CurrentUser"] != null)
+            {
+                u = (User)Session["CurrentUser"];                
+            }
+            else
+            {
+                Session["CurrentUser"] = u;
+            }
+
+            ViewBag.FirstName = u.FirstName;
+            ViewBag.LastName = u.LastName;
+            ViewBag.Password = u.Password;
+            ViewBag.Email = u.Email;
+            ViewBag.PhoneNumber = u.PhoneNumber;
+            return View();
+        }
+    }
+}
